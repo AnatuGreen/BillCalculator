cb3511c34c907cc4416211481852f487d7cc793f
Anaturuchi <anatuonline@gmail.com>
2022-01-14 10:15:13 +0100

    Update bill_calc.js

--- bill_calc.js
+++ bill_calc.js
@@ -13,6 +13,7 @@ let Joshua = document.getElementById("Joshua") // 9
 let Lawrence = document.getElementById("Lawrence") // 7
 let Ogundairo = document.getElementById("Ogundairo") //10
 let Taiwo = document.getElementById("Taiwo") //6
+let Victor = document.getElementByID("Victor") // 5
 
 let how = document.getElementById("how")
 let howItIs = document.getElementById("howitis")
@@ -20,7 +21,7 @@ let hideHowItIs = document.getElementById("hideHowItIs")
 
 function total(){
     
-    let totalPoints= 85
+    let totalPoints= 90
     let billInput1 = document.getElementById("bill1").value || 0
     let billInput2 = document.getElementById("bill2").value || 0
     let totalBillDisplayArea = document.getElementById("total")
@@ -42,6 +43,8 @@ function total(){
     Lawrence.textContent = "Lawrence's Electric Bill (7 Points) is: N" + Math.floor(amountPerPoint * 7 + 1).toLocaleString() 
     Ogundairo.textContent = "Ogundairo's Electric Bill (10 Points) is: N" + Math.floor(amountPerPoint * 10 + 1).toLocaleString()
     Taiwo.textContent = "Taiwo's Electric Bill (6 Points) is: N" + Math.floor(amountPerPoint * 6 + 1).toLocaleString()
+    Victor.textContent = "Victor's Electric Bill (5 Points) is: N" + Math.floor(amountPerPoint * 5 + 1).toLocaleString()
+
 
     //// Math.floor( .... + 1) to round off the amount without decimals and add 1 to avoid financial loss on our side. toLocalString() adds the comma to make it a readable thousand
 }
